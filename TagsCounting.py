2b5c3f9df433baaebcc5d2072897dc18b8f6824a
Cr0nium <Cr0n4@mail.ru>
2019-06-15 17:01:49 +0300

    working version 1.0

--- TagsCounting.py
+++ TagsCounting.py
@@ -11,6 +11,7 @@ if __name__ == '__main__':
         .getOrCreate()
 
     spark.sparkContext.setLogLevel("INFO")
+    spark.conf.set("spark.sql.objectHashAggregate.sortBased.fallbackThreshold", 6000)
 
     schemaLong = StructType([
         StructField("ts", TimestampType(), True),
@@ -24,19 +25,33 @@ if __name__ == '__main__':
         StructField("roll_end", TimestampType(), True)
     ])
 
-    longDF = spark.read\
-        .schema(schemaLong)\
-        .option("header", "true")\
-        .option("delimiter", ";")\
-        .csv("C:\\Users\\timur\\PycharmProjects\\Severstal_test\\long.csv")
+    longDF = spark.read \
+        .schema(schemaLong) \
+        .option("header", "true") \
+        .option("delimiter", ";") \
+        .csv("C:\\Users\\timur\\PycharmProjects\\Severstal_test\\long_short.csv") \
+        .repartitionByRange(48, 'ts')
 
     rollsDF = spark.read \
         .schema(schemaRolls) \
         .option("header", "true") \
         .option("delimiter", ";") \
-        .csv("C:\\Users\\timur\\PycharmProjects\\Severstal_test\\rolls.csv")
+        .csv("C:\\Users\\timur\\PycharmProjects\\Severstal_test\\rolls_short.csv")
 
-    rollsDF.show(truncate=False)
+    rollsAndLongRightJoinDF = longDF.join(rollsDF, (rollsDF.roll_start <= longDF.ts) & (longDF.ts <= rollsDF.roll_end), how='right')\
+        .drop("ts", "roll_start", "roll_end")
+    rollsAndLongRightJoinDF.createOrReplaceTempView("tmp_view")
+    resultAggDF = spark.sql(
+        "select roll_id, tag, max(value) as max, mean(value) as mean, percentile_approx(value, 0.5) as median, " +
+        "percentile_approx(value, 0.99) as 99_percentile, percentile_approx(value, 0.01) as 1_percentile from tmp_view group by roll_id, tag order by roll_id")
+
+    resultAggDF \
+        .orderBy("roll_id") \
+        .coalesce(1) \
+        .write \
+        .format("csv") \
+        .option("header", "true") \
+        .save("H:\\res\\finalPython1")
 
     spark.stop()
     print("Application Completed")
