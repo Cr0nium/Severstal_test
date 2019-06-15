10659f26c26edf257821d136cab11e9427ca4448
Cr0nium <Cr0n4@mail.ru>
2019-06-15 11:37:57 +0300

    add csv files

--- TagsCounting.py
+++ TagsCounting.py
@@ -1,3 +1,12 @@
+from pyspark.sql import SparkSession
+
 if __name__ == '__main__':
     print("Start Application")
 
+    spark = SparkSession\
+        .builder\
+        .appName("Severstal_test")\
+        .master("local[*]")\
+        .getOrCreate()
+    rollDF = spark.read.csv("C:\\Users\\timur\\PycharmProjects\\Severstal_test\\rolls.csv")
+    rollDF.show()
