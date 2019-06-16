4d581677a528a4f7a0ff3603c6277720cb19ffde
Cr0nium <Cr0n4@mail.ru>
2019-06-16 14:32:15 +0300

    final optimized Python version 1.15 (15 optimized tests complete) best run time 25:31

--- TagsCounting.py
+++ TagsCounting.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import broadcast
 from pyspark.sql.types import StructType, TimestampType, LongType, StringType, StructField
 
 if __name__ == '__main__':
@@ -8,10 +9,13 @@ if __name__ == '__main__':
         .builder \
         .appName("Severstal_test") \
         .master("local[*]") \
+        .config("spark.driver.memory", "6g") \
+        .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer") \
+        .config("spark.sql.objectHashAggregate.sortBased.fallbackThreshold", 1500) \
+        .config("spark.default.parallelism", 48)\
         .getOrCreate()
 
     spark.sparkContext.setLogLevel("INFO")
-    spark.conf.set("spark.sql.objectHashAggregate.sortBased.fallbackThreshold", 3000)
 
     schemaLong = StructType([
         StructField("ts", TimestampType(), True),
@@ -38,13 +42,25 @@ if __name__ == '__main__':
         .option("delimiter", ";") \
         .csv("C:\\Users\\timur\\PycharmProjects\\Severstal_test\\rolls.csv")
 
-    rollsAndLongRightJoinDF = longDF.join(rollsDF, (rollsDF.roll_start <= longDF.ts) & (longDF.ts <= rollsDF.roll_end), how='right')\
+    rollsAndLongRightJoinDF = longDF.join(broadcast(rollsDF), (rollsDF.roll_start <= longDF.ts) & (longDF.ts <= rollsDF.roll_end), how='right') \
         .drop("ts", "roll_start", "roll_end")
-
     rollsAndLongRightJoinDF.createOrReplaceTempView("tmp_view")
+
     resultAggDF = spark.sql(
-        "select roll_id, tag, max(value) as max, mean(value) as mean, percentile_approx(value, 0.5) as median, " +
-        "percentile_approx(value, 0.99) as 99_percentile, percentile_approx(value, 0.01) as 1_percentile from tmp_view group by roll_id, tag order by roll_id")
+        "select " +
+        "   roll_id, "
+        "   tag, "
+        "   max(value) as max, "
+        "   mean(value) as mean, "
+        "   percentile_approx(value, 0.5) as median, "
+        "   percentile_approx(value, 0.99) as 99_percentile, "
+        "   percentile_approx(value, 0.01) as 1_percentile "
+        "from "
+        "   tmp_view "
+        "group by "
+        "   roll_id, tag "
+        "order by "
+        "   roll_id")
 
     resultAggDF \
         .orderBy("roll_id") \
@@ -53,7 +69,7 @@ if __name__ == '__main__':
         .option("nullValue", None) \
         .format("csv") \
         .option("header", "true") \
-        .save("H:\\res\\finalPython10")
+        .save("H:\\res\\finalPython")
 
     spark.stop()
     print("Application Completed")
